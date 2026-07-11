044c042768896cee1e4bbabb493e61cef2af37dc
wmxzrs <wmxzrs@users.noreply.github.com>
2026-07-12 02:24:51 +0800

    fix: eslint config, remove CDN from HTML template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
   root: true,
-  env: {
-    node: true,
-  },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
+  env: { node: true },
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
   rules: {
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'warn',
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
   },
 }
