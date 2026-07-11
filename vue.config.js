37e4fd42f3eb4dee0808a998617bd93882cf3094
wmxzrs <wmxzrs@users.noreply.github.com>
2026-07-12 02:21:55 +0800

    fix: add jquery/leancloud deps, webpack ProvidePlugin for $

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
+const webpack = require('webpack')
+
 module.exports = {
   productionSourceMap: false,
   publicPath: '/',
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.$': 'jquery',
+        'window.jQuery': 'jquery',
+      }),
+    ],
+  },
   css: {
     loaderOptions: {
       sass: {
