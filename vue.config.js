c1ab4bed52e28bb994b4615ef5b95f1f40485090
wmxzrs <wmxzrs@users.noreply.github.com>
2026-07-12 02:17:14 +0800

    fix: dart-sass compat - /deep/ → ::v-deep, @import → @use

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,8 @@ module.exports = {
     loaderOptions: {
       sass: {
         additionalData: `
-          @import "~@/styles/variables.scss";
-          @import "~@/styles/mixin.scss";
+          @use "~@/styles/variables.scss" as *;
+          @use "~@/styles/mixin.scss" as *;
         `,
         sassOptions: {
           silenceDeprecations: ['legacy-js-api'],
